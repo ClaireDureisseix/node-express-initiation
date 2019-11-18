b6d3cf5a5240d2658b08a955320255226deba809
Claire <clairepelletier.prod@gmail.com>
2019-11-18 10:49:53 +0100

    done: route written

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (request, response) => {
-  response.send('Welcome to Express');
+app.get('/api/movies', (request, response) => {
+  response.send('All films');
 });
+
+app.get('/api/movies/:id', (req, res) => {
+  res.json({ result: '10 results found' })
+});
+
+app.get(`/api/employee`, (req, res) => {
+  res.sendStatus(304);
+});
+
+app.get(`/api/employee?name=/:name`, (req, res) => {
+  res.status(404).send('Unable to retrieve /api/employee?name=/:name');
+});
+
 
 app.listen(port, (err) => {
   if (err) {
